f075f5861a44c1384a7cd4f27a67bb354026c10f
Theophile-Yvars <79571637+Theophile-Yvars@users.noreply.github.com>
2022-11-23 19:26:03 +0100

    [DEV] Process run #2

--- Server/RoutingServer/ConsoleApp_for_Self_Hosted_WS/Program.cs
+++ Server/RoutingServer/ConsoleApp_for_Self_Hosted_WS/Program.cs
@@ -57,35 +57,8 @@ namespace MyRoutingServer
             string adressA = "33 Rue Edouard Nieuport, 69008 Lyon";
             string adressB = "4 Pl. du Marché, 69009 Lyon";
 
-            /**
-             * 1. Calls OpenStreetMap to retrieve information about the
-             * given address.
-             */
-
-            OSMProcess osmProcess = new OSMProcess();
-            osmProcess.run(adressA, adressB);
-            osmProcess.printOSMCoordiante();
-
-
-            /**
-             * 2. Calls JCDecaux to retrieve the stations and find: the
-             * closest one from the origin with available bikes, and the
-             * closest from the destination with places to drop the bike.
-             */
-
-            JCDecauxProcess jCDecauxProcess = new JCDecauxProcess();
-            jCDecauxProcess.run(osmProcess.OSMCoordinateA, osmProcess.OSMCoordinateB);
-            jCDecauxProcess.printJCDevauxCoordinate();
-
-            /*
-             * 3.If there are stations matching the conditions, calls
-             * OpenStreetMap for 3 itineraries: Origin to Station1,
-             * Station1 to Station2, Station2 to Destination
-             */
-
-            //https://api.openrouteservice.org/v2/directions/driving-car?api_key=your-api-key&start=8.681495,49.41461&end=8.687872,49.420318
-            DirectionProcess directionProcess = new DirectionProcess();
-            directionProcess.run(osmProcess.positionA, jCDecauxProcess.positionA, jCDecauxProcess.positionB, osmProcess.positionB);
+            ProcessOfObtainingTheSteps process = new ProcessOfObtainingTheSteps();
+            process.run(adressA, adressB);
 
             Console.ReadLine();
         }
