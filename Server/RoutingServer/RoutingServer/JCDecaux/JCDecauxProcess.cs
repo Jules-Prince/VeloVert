2af670a3370ab8616cd2bd41e6a3d2f87cd8233e
Theophile-Yvars <79571637+Theophile-Yvars@users.noreply.github.com>
2022-12-06 11:41:34 +0100

    [DOC] some comments

--- Server/RoutingServer/RoutingServer/JCDecaux/JCDecauxProcess.cs
+++ Server/RoutingServer/RoutingServer/JCDecaux/JCDecauxProcess.cs
@@ -11,9 +11,12 @@ namespace RoutingServer
 {
     public class JCDecauxProcess
     {
-
-        public Position positionA { set; get; }
-        public Position positionB { set; get; }
+        /**
+         * This class is responsible for the communication with the proxy, 
+         * in order to retrieve the stations of a city. 
+         */
+        public Position positionA { set; get; } // Position of the closest station to A
+        public Position positionB { set; get; } // Position of the closest station to B
 
         public JCDecauxProcess()
         {
@@ -21,23 +24,24 @@ namespace RoutingServer
             this.positionB = new Position();
         }
 
+        /**
+         * Allows you to run the process of finding the closest location to point A and point B. 
+         * This method returns a boolean. If there is no station in the city, then it returns false. Otherwise it returns true. 
+         */
         public Boolean run(string cityA, double latitudeA, double longitudeA, string cityB, double latitudeB, double longitudeB)
         {
             string urlAPI = "https://api.jcdecaux.com/vls/v3/stations?apiKey=56c5d019b4d68c1bd60800a1345b299bc7bb95b0&contract=";
 
-            // 1 Récuperer la listes des stations avec le nom de la ville
+            // 1 Retrieve the list of stations with the name of the city
             List<RootJCDecauxItem> rootA = buildDeserializedClass(urlAPI, cityA);
-            //printLatitudeLongitudeStation(rootA);
-
             List<RootJCDecauxItem> rootB = buildDeserializedClass(urlAPI, cityB);
-            //printLatitudeLongitudeStation(rootB);
 
             if (rootA.Count == 0 || rootB.Count == 0)
             {
                 return false;
             }
 
-            // 2 Chercher la station la plus proche avec les coordonnées gps
+            // 2 Search for the nearest station with gps coordinates
             RootJCDecauxItem rootJCDecauxDataA = findStationMoreClosed(rootA, longitudeA, latitudeA, Direction.Start);
             RootJCDecauxItem rootJCDecauxDataB = findStationMoreClosed(rootB, longitudeB, latitudeB, Direction.End);
 
@@ -46,7 +50,7 @@ namespace RoutingServer
                 return false;
             }
 
-            // 3 J'enregistre les coordonnées dans la classe adéquate.
+            // 3 I save the coordinates in the variables of the class. 
             this.positionA.latitude = rootJCDecauxDataA.position.latitude;
             this.positionA.longitude = rootJCDecauxDataA.position.longitude;
 
@@ -56,6 +60,15 @@ namespace RoutingServer
             return true;
         }
 
+        /**
+         * This method returns the Item (the return object of the JCDecaux api) corresponding 
+         * to the closest station to the given position. 
+         * 
+         * The direction is used to know if it is to come to take a bike
+         * (So it is to know if there is a bike in the station)
+         * , or to come to drop one 
+         * (So it is to know if there is space left in the sta
+         */
         private RootJCDecauxItem findStationMoreClosed(List<RootJCDecauxItem> root, double longitude, double latitude, Direction direction)
         {
             RootJCDecauxItem myStation = null;
@@ -83,6 +96,11 @@ namespace RoutingServer
             return myStation;
         }
 
+        /**
+         * This method filters the list of stations in a city, depending on the direction. 
+         * If it's to pick up a bike, there must be at least one left. 
+         * If it's to drop off a bike, there must be at least one space available. 
+         */
         private List<RootJCDecauxItem> deletingInvalidStations(Direction direction, List<RootJCDecauxItem> root)
         {
             List<RootJCDecauxItem> newList = new List<RootJCDecauxItem>();
@@ -120,15 +138,24 @@ namespace RoutingServer
             End
         }
 
+        /**
+         * To compare distances, and to find the closest station, we use the Pythagorian theorem. 
+         */
         private double distancePythagore(RootJCDecauxItem rootJC, double longitude, double latitude)
         {
-            // Pythagore 
+            // Pythagore : a² = b² + c²
             double x = Math.Abs(longitude - rootJC.position.longitude);
             double y = Math.Abs(latitude - rootJC.position.latitude);
             double d = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
             return d;
         }
 
+        /**
+         * This method asks the proxy to make a query, to get the stations of a city, 
+         * then returns the object containing the information. 
+         * ( In the proxy, the return json is deserialized and stored as an object ).
+         * If there is no response from the proxy, returns an object without content.  
+         */
         private List<RootJCDecauxItem> buildDeserializedClass(string urlAPI, string param)
         {
             ApiManager aPIManager = new ApiManager();
@@ -146,6 +173,10 @@ namespace RoutingServer
             }
         }
 
+        /**
+         * This method converts the return object of the proxy into a usable object in this class. 
+         * It is just an adapter
+         */
         private List<RootJCDecauxItem> convert(RoutingServer.MyProxy.RootJCDecauxItem[] list)
         {
             List<RootJCDecauxItem> myNewList = new List<RootJCDecauxItem>();
@@ -156,18 +187,8 @@ namespace RoutingServer
         }
 
         /**
-         *  For DEBUG
+         * This method displays the coordinates of the 2 stations. 
          */
-        private void printLatitudeLongitudeStation(List<RootJCDecauxDataAPI> rootA)
-        {
-            foreach (RootJCDecauxDataAPI r in rootA)
-            {
-                Console.WriteLine("Latitude : " + r.position.latitude);
-                Console.WriteLine("Longitude : " + r.position.longitude);
-                Console.WriteLine("");
-            }
-        }
-
         public void printJCDevauxCoordinate()
         {
             Console.WriteLine("JCDevaux Coordinate A");
--- Server/RoutingServer/RoutingServer/OpenStreetMap/OSMCoordinate.cs
+++ Server/RoutingServer/RoutingServer/OpenStreetMap/OSMCoordinate.cs
@@ -8,6 +8,10 @@ namespace RoutingServer
 {
     public class OSMCoordinate
     {
+        /**
+         * This class is a POJO that contains all the useful information about the search 
+         * of a city with its information via open street map. 
+         */
         public double latitude { get; set; }
         public double longitude { get; set; }
         public string city { get; set; }
